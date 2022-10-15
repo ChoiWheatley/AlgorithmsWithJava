4f5618f020c13134d561ad1e2ea36b86952ef11e
ChoiWheatley <chltmdgus604@naver.com>
2022-10-15 15:34:57 +0900

    이전 solution + exceptWords

--- Ex120956.java
+++ Ex120956.java
@@ -38,75 +38,21 @@ public class Ex120956 {
 
   public static int solution(String[] babbling) {
     var answer = 0;
-    // for (var b : babbling) {
-    // StringBuilder babble = new StringBuilder(b);
-    // List<String> mutableWords = Arrays.asList(words);
-    // for (var word : words) {
-    // var startidx = babble.indexOf(word);
-    // if (startidx != -1) {
-    // var endidx = startidx + word.length();
-    // babble.delete(startidx, endidx);
-    // }
-    // }
-    // if (babble.length() == 0) {
-    // answer++;
-    // }
-    // }
-
     for (var b : babbling) {
-      var found = false;
-      while (!found) {
+      StringBuilder babble = new StringBuilder(b);
+      List<String> mutableWords = Arrays.asList(words);
+      for (var word : words) {
+        var startidx = babble.indexOf(word);
+        if (startidx != -1) {
+          var endidx = startidx + word.length();
+          babble.delete(startidx, endidx);
+        }
       }
-      if (found) {
+      if (babble.length() == 0) {
         answer++;
       }
     }
 
     return answer;
-  }
-
-  @Test
-  public void testAssess1() {
-    List<String> words = Arrays.asList("aa", "bb", "cc");
-    String babble = "aabbcc";
-    assertEquals(true, assess(babble, words));
-  }
-
-  @Test
-  public void testAssess2() {
-    List<String> words = Arrays.asList("aa", "bb", "cc");
-    String babble = "aabbcccc";
-    assertEquals(false, assess(babble, words));
-  }
-
-  public static boolean assess(String babble, List<String> words) {
-    var ret = true;
-    for (var w : words) {
-      var remain = deleteWord(babble, w);
-      if (remain.equals(babble)) {
-        // not found
-        continue;
-      }
-      // found
-      List<String> tmpWords = new ArrayList<>(words);
-      tmpWords.remove(w);
-      ret = assess(remain, tmpWords);
-    }
-    return ret;
-  }
-
-  /**
-   * 문자열에 내가 원하는 단어를 제거한다.
-   * 
-   * @param babble 평가 하려는 문자열
-   * @param word   비교
-   * @return babble에서 찾은 첫 번째 word를 제거한 문자열
-   */
-  public static String deleteWord(String babble, String word) {
-    var startidx = babble.indexOf(word);
-    var endidx = startidx + word.length();
-    var ret = new StringBuilder(babble);
-    ret.delete(startidx, endidx);
-    return ret.toString();
   }
 }
