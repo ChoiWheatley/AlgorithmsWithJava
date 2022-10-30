6a9451863e2b36984b3991379adebece050f46d0
ChoiWheatley <chltmdgus604@naver.com>
2022-10-31 00:33:29 +0900

    Test 클래스 따로 빼놓았다.

--- acmicpc/Ex24060.java
+++ acmicpc/Ex24060.java
@@ -1,7 +1,5 @@
 package acmicpc;
 
-import static org.junit.Assert.assertEquals;
-
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
@@ -11,11 +9,8 @@ import java.util.Comparator;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Random;
 import java.util.stream.Stream;
 import java.util.Map;
-
-import org.junit.Test;
 
 public class Ex24060 {
   public static void main(String[] args) throws IOException {
@@ -34,41 +29,7 @@ public class Ex24060 {
 
     var solver = new Solver();
     var answer = solver.solve(unordered, k);
-    // bw.write(answer + "\n");
-    System.out.println(answer);
-  }
-
-  @Test
-  public void testcase1() {
-    /**
-     * 5 7
-     * 4 5 1 3 2
-     */
-    var unordered = new Long[] { 4l, 5l, 1l, 3l, 2l };
-    var k = 7;
-    var solver = new Solver();
-    assertEquals(3L, solver.solve(unordered, k));
-  }
-
-  @Test
-  public void loongRandomStreamSpeedTest() throws IOException {
-    Long[] unordered = (new Random()).ints(500_000)
-        .boxed()
-        .toArray(Long[]::new);
-    System.out.println(unordered[unordered.length - 1]);
-  }
-
-  @Test
-  public void timeoutTest1() {
-    /**
-     * 500_000개의 원소를 정렬할 때 과연 얼마나 오래 걸리는지
-     */
-    Long[] unordered = (new Random()).longs(50_000)
-        .boxed()
-        .toArray(Long[]::new);
-    var k = (int) Math.pow(10, 8);
-    var solver = new Solver();
-    solver.solve(unordered, k);
+    bw.write(answer + "\n");
   }
 }
 
---
+++ acmicpc/Test24060.java
@@ -0,0 +1,43 @@
+package acmicpc;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.IOException;
+import java.util.Random;
+
+import org.junit.Test;
+
+public class Test24060 {
+  @Test
+  public void testcase1() {
+    /**
+     * 5 7
+     * 4 5 1 3 2
+     */
+    var unordered = new Long[] { 4l, 5l, 1l, 3l, 2l };
+    var k = 7;
+    var solver = new Solver();
+    assertEquals(3L, solver.solve(unordered, k));
+  }
+
+  @Test
+  public void loongRandomStreamSpeedTest() throws IOException {
+    Long[] unordered = (new Random()).longs(500_000)
+        .boxed()
+        .toArray(Long[]::new);
+    System.out.println(unordered[unordered.length - 1]);
+  }
+
+  @Test
+  public void timeoutTest1() {
+    /**
+     * 500_000개의 원소를 정렬할 때 과연 얼마나 오래 걸리는지
+     */
+    Long[] unordered = (new Random()).longs(50_000)
+        .boxed()
+        .toArray(Long[]::new);
+    var k = (int) Math.pow(10, 8);
+    var solver = new Solver();
+    solver.solve(unordered, k);
+  }
+}
