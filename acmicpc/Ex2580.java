2b067a1ff8af0877f320c27a841042b210beadea
ChoiWheatley <chltmdgus604@naver.com>
2022-11-11 23:04:53 +0900

    start 2580

---
+++ acmicpc/Ex2580.java
@@ -0,0 +1,102 @@
+package acmicpc;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+public class Ex2580 {
+  public static void main(String[] args) {
+
+  }
+}
+
+class Solver2580 {
+  public static int[][] solve(int[][] unfinished) {
+    int[][] ret = unfinished.clone();
+
+    return ret;
+  }
+
+  public static boolean validate(int[][] sudoku) {
+    // every rows
+    for (int row = 0; row < sudoku.length; ++row) {
+      IntStream line = IntStream.of(sudoku[row]);
+      if (!checkOne2Nine(line))
+        return false;
+    }
+    // every cols
+    int[][] transposed = transpose(sudoku);
+    for (int col = 0; col < transposed.length; ++col) {
+      IntStream line = IntStream.of(transposed[col]);
+      if (!checkOne2Nine(line))
+        return false;
+    }
+    // every 3x3 boxes
+    return true;
+  }
+
+  public static boolean checkOne2Nine(IntStream stream) {
+    boolean[] checklist = new boolean[10];
+    checklist[0] = true; // not used
+    stream.forEach(e -> checklist[e] = true);
+    for (boolean each : checklist) {
+      if (!each)
+        return false;
+    }
+    return true;
+  }
+
+  // i행과 j열을 뒤바꾼다.
+  public static int[][] transpose(int[][] origin) {
+    int[][] ret = new int[origin.length][origin[0].length];
+    for (int j = 0; j < origin.length; ++j) {
+      for (int i = 0; i < origin[0].length; ++i) {
+        ret[i][j] = origin[j][i];
+      }
+    }
+    return ret;
+  }
+
+  /**
+   * (row, col) 위치에서부터 3x3 소행렬을 구해 리턴한다.
+   * 
+   * @param origin 9x9 스도쿠
+   */
+  public static int[] get3x3StartWith(int row, int col, int[][] origin) {
+    int[] ret = new int[Const2580.LEN];
+    int cnt = 0;
+    for (int rowCnt = 0; rowCnt < 3; ++rowCnt) {
+      for (int colCnt = 0; colCnt < 3; ++colCnt) {
+        var sample = origin[row + rowCnt][col + colCnt];
+        ret[cnt++] = sample;
+      }
+    }
+    return ret;
+  }
+
+}
+
+class SudokuStringConverter {
+  public static int[][] convert(String lines) {
+    String[] splitted = lines.split("\n");
+    return convert(splitted);
+  }
+
+  public static int[][] convert(String[] lines) {
+    int[][] ret = new int[Const2580.LEN][Const2580.LEN];
+    for (int row = 0; row < lines.length; ++row) {
+      String line = lines[row];
+      String[] splitted = line.split(" ");
+      int[] converted = Stream.of(splitted).mapToInt(Integer::valueOf).toArray();
+      for (int col = 0; col < converted.length; ++col) {
+        ret[row][col] = converted[col];
+      }
+    }
+    return ret;
+  }
+}
+
+class Const2580 {
+  public static final int LEN = 9;
+}
---
+++ acmicpc/Test2580.java
@@ -0,0 +1,96 @@
+package acmicpc;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+import java.util.stream.IntStream;
+
+import org.junit.Test;
+
+public class Test2580 {
+  public static String sampleStr = """
+      0 3 5 4 6 9 2 7 8
+      7 8 2 1 0 5 6 0 9
+      0 6 0 2 7 8 1 3 5
+      3 2 1 0 4 6 8 9 7
+      8 0 4 9 1 3 5 0 6
+      5 9 6 8 2 0 4 1 3
+      9 1 7 6 5 2 0 8 0
+      6 0 3 7 0 1 9 5 2
+      2 5 8 3 9 4 7 6 0""";
+  public static String answerStr = """
+      1 3 5 4 6 9 2 7 8
+      7 8 2 1 3 5 6 4 9
+      4 6 9 2 7 8 1 3 5
+      3 2 1 5 4 6 8 9 7
+      8 7 4 9 1 3 5 2 6
+      5 9 6 8 2 7 4 1 3
+      9 1 7 6 5 2 3 8 4
+      6 4 3 7 8 1 9 5 2
+      2 5 8 3 9 4 7 6 1""";
+
+  @Test
+  public void converterTest() {
+    int[][] answer = new int[][] {
+        { 0, 3, 5, 4, 6, 9, 2, 7, 8 },
+        { 7, 8, 2, 1, 0, 5, 6, 0, 9 },
+        { 0, 6, 0, 2, 7, 8, 1, 3, 5 },
+        { 3, 2, 1, 0, 4, 6, 8, 9, 7 },
+        { 8, 0, 4, 9, 1, 3, 5, 0, 6 },
+        { 5, 9, 6, 8, 2, 0, 4, 1, 3 },
+        { 9, 1, 7, 6, 5, 2, 0, 8, 0 },
+        { 6, 0, 3, 7, 0, 1, 9, 5, 2 },
+        { 2, 5, 8, 3, 9, 4, 7, 6, 0 },
+    };
+
+    assertArrayEquals(answer, SudokuStringConverter.convert(sampleStr));
+  }
+
+  @Test
+  public void checkOne2NineTest() {
+    var stream = IntStream.of(1, 2, 3, 4, 5, 6, 7, 8, 9);
+    var stream2 = IntStream.range(1, 10);
+    assertEquals(true, Solver2580.checkOne2Nine(stream));
+    assertEquals(true, Solver2580.checkOne2Nine(stream2));
+    var stream3 = IntStream.of(0, 3, 5, 4, 6, 9, 2, 7, 8);
+    assertEquals(false, Solver2580.checkOne2Nine(stream3));
+    var stream4 = IntStream.of(1, 3, 5, 4, 6, 9, 2, 7, 8);
+    assertEquals(true, Solver2580.checkOne2Nine(stream4));
+  }
+
+  @Test
+  public void validationTest() {
+    int[][] sampleArr = SudokuStringConverter.convert(sampleStr);
+    assertEquals(true, Solver2580.validate(sampleArr));
+  }
+
+  @Test
+  public void get3x3Test() {
+    int[][] sampleArr = SudokuStringConverter.convert(answerStr);
+    var start0x0 = new int[] { 1, 3, 5, 7, 8, 2, 4, 6, 9 };
+    var start0x3 = new int[] { 4, 6, 9, 1, 3, 5, 2, 7, 8 };
+    var start0x6 = new int[] { 2, 7, 8, 6, 4, 9, 1, 3, 5 };
+    var start3x0 = new int[] { 3, 2, 1, 8, 7, 4, 5, 9, 6 };
+    var start3x3 = new int[] { 5, 4, 6, 9, 1, 3, 8, 2, 7 };
+    var start3x6 = new int[] { 8, 9, 7, 5, 2, 6, 4, 1, 3 };
+    var start6x0 = new int[] { 9, 1, 7, 6, 4, 3, 2, 5, 8 };
+    var start6x3 = new int[] { 6, 5, 2, 7, 8, 1, 3, 9, 4 };
+    var start6x6 = new int[] { 3, 8, 4, 9, 5, 2, 7, 6, 1 };
+
+    assertArrayEquals(start0x0, Solver2580.get3x3StartWith(0, 0, sampleArr));
+    assertArrayEquals(start0x3, Solver2580.get3x3StartWith(0, 3, sampleArr));
+    assertArrayEquals(start0x6, Solver2580.get3x3StartWith(0, 6, sampleArr));
+    assertArrayEquals(start3x0, Solver2580.get3x3StartWith(3, 0, sampleArr));
+    assertArrayEquals(start3x3, Solver2580.get3x3StartWith(3, 3, sampleArr));
+    assertArrayEquals(start3x6, Solver2580.get3x3StartWith(3, 6, sampleArr));
+    assertArrayEquals(start6x0, Solver2580.get3x3StartWith(6, 0, sampleArr));
+    assertArrayEquals(start6x3, Solver2580.get3x3StartWith(6, 3, sampleArr));
+    assertArrayEquals(start6x6, Solver2580.get3x3StartWith(6, 6, sampleArr));
+  }
+
+  @Test
+  public void validationTest2() {
+    int[][] sampleArr = SudokuStringConverter.convert(sampleStr);
+    assertEquals(false, Solver2580.validate(sampleArr));
+  }
+}
