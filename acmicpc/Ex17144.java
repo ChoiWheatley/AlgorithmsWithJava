4cd286b061afd0485e732181d19a798c9b638839
ChoiWheatley <chltmdgus604@naver.com>
2022-11-18 19:31:28 +0900

    [17144] 이상한 임포트 문장 제거

--- acmicpc/Ex17144.java
+++ acmicpc/Ex17144.java
@@ -3,8 +3,6 @@ package acmicpc;
 import java.util.ArrayList;
 import java.util.List;
 
-import acmicpc.AbstractCellFactory.InvalidPurifiers;
-
 public class Ex17144 {
   public static void main(String[] args) {
 
