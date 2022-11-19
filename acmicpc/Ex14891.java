fffaef02686c89cb86c244ed8877ab041c8b1a95
ChoiWheatley <chltmdgus604@naver.com>
2022-11-19 17:07:45 +0900

    [14891] start

---
+++ acmicpc/Ex14891.java
@@ -0,0 +1,19 @@
+package acmicpc;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Ex14891 {
+  public static void main(String[] args) {
+    try (var br = new BufferedReader(new InputStreamReader(System.in));
+        var bw = new BufferedWriter(new OutputStreamWriter(System.out))) {
+      String line = br.readLine();
+
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+}
