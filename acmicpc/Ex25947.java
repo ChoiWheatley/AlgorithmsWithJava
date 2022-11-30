7f67fade3b6d8bc0fe903d9ae367abddfeedfdb7
ChoiWheatley <chltmdgus604@naver.com>
2022-12-01 02:52:14 +0900

    [25947] 와 int형이라서 계속 틀린 거였어

--- acmicpc/Ex25947.java
+++ acmicpc/Ex25947.java
@@ -1,37 +1,30 @@
 package acmicpc;
 
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 import java.util.stream.Stream;
 
 public class Ex25947 {
-  public static void main(String[] args) {
-    try (var br = new BufferedReader(new InputStreamReader(System.in));
-        var bw = new BufferedWriter(new OutputStreamWriter(System.out))) {
-      String line = br.readLine();
-      int[] ints = Stream.of(line.split(" ")).mapToInt(Integer::valueOf).toArray();
-      int n = ints[0];
-      int b = ints[1];
-      int a = ints[2];
-      int[] prices = new int[n];
+  public static void main(String[] args) throws IOException {
+    var br = new BufferedReader(new InputStreamReader(System.in));
+    String line = br.readLine();
+    int[] ints = Stream.of(line.split(" ")).mapToInt(Integer::valueOf).toArray();
+    int n = ints[0];
+    int b = ints[1];
+    int a = ints[2];
+    long[] prices = new long[n];
 
-      line = br.readLine();
-      var tokenizer = new StringTokenizer(line);
-      for (int i = 0; i < n; ++i) {
-        prices[i] = Integer.valueOf(tokenizer.nextToken());
-      }
-
-      int result = Solver25947Alt2.solve(prices, b, a);
-      bw.write(result + "\n");
-
-    } catch (IOException e) {
-      e.printStackTrace();
+    line = br.readLine();
+    var tokenizer = new StringTokenizer(line);
+    for (int i = 0; i < n; ++i) {
+      prices[i] = Long.valueOf(tokenizer.nextToken());
     }
+
+    int result = Solver25947Alt4.solve(prices, b, a);
+    System.out.println(result);
   }
 }
 
@@ -44,7 +37,7 @@ class Solver25947 {
    * 
    * @return 최대로 살 수 있는 선물의 수
    */
-  public static int solve(int[] prices, int budget, int coupon) {
+  public static int solve(long[] prices, int budget, int coupon) {
     Arrays.sort(prices);
     int count = 0;
     for (int i = 0; i < prices.length; ++i) {
@@ -53,7 +46,7 @@ class Solver25947 {
     return count;
   }
 
-  public static int recursion(int[] pricesSorted, int remainBudget, int remainCoupon, int idx, int count) {
+  public static int recursion(long[] pricesSorted, int remainBudget, int remainCoupon, int idx, int count) {
     if (remainCoupon < 0)
       return count - 1;
     if (idx >= pricesSorted.length)
@@ -62,12 +55,12 @@ class Solver25947 {
       return count - 1;
     int curCount = 0;
     for (int i = idx; i < pricesSorted.length; ++i) {
-      int price = pricesSorted[i];
+      var price = pricesSorted[i];
       if (remainBudget - price < 0) {
         price /= 2;
         remainCoupon--;
       }
-      int result = recursion(pricesSorted, remainBudget - price, remainCoupon, idx + 1, count + 1);
+      int result = recursion(pricesSorted, remainBudget - (int) price, remainCoupon, idx + 1, count + 1);
       if (result <= curCount)
         break;
       else
@@ -79,30 +72,30 @@ class Solver25947 {
 
 class Solver25947Alt1 {
 
-  public static int solve(int[] prices, int budget, int coupon) {
+  public static int solve(long[] prices, int budget, int coupon) {
     Arrays.sort(prices);
     return recursion(prices, budget, coupon, 0, 0);
   }
 
-  public static int recursion(int[] pricesSorted, int remainBudget, int remainCoupon, int idx, int count) {
+  public static int recursion(long[] pricesSorted, int remainBudget, int remainCoupon, int idx, int count) {
     if (remainCoupon < 0)
       return count - 1;
     if (idx >= pricesSorted.length)
       return count - 1;
     if (remainBudget < 0)
       return count - 1;
-    int price = pricesSorted[idx];
+    var price = pricesSorted[idx];
     // 쿠폰을 사용했을 때의 결과
     int yesCoupon = recursion(
         pricesSorted,
-        remainBudget - (price / 2),
+        remainBudget - (int) (price / 2),
         remainCoupon - 1,
         idx + 1,
         count + 1);
     // 쿠폰을 사용하지 않았을 때의 결과
     int noCoupon = recursion(
         pricesSorted,
-        remainBudget - price,
+        remainBudget - (int) price,
         remainCoupon,
         idx + 1,
         count + 1);
@@ -115,26 +108,26 @@ class Solver25947Alt1 {
 
 class Solver25947Alt2 {
 
-  public static int solve(int[] prices, int budget, int coupon) {
+  public static long solve(long[] prices, int budget, int coupon) {
     Arrays.sort(prices);
     return (new Solver25947Alt2()).recursion(prices, budget, coupon, 0, 0);
   }
 
-  private int best = 0;
+  private long best = 0;
 
-  public int recursion(int[] pricesSorted, int remainBudget, int remainCoupon, int idx, int count) {
+  public long recursion(long[] pricesSorted, int remainBudget, int remainCoupon, int idx, int count) {
     if (remainCoupon < 0)
       return count - 1;
     if (idx >= pricesSorted.length)
       return count - 1;
     if (remainBudget < 0)
       return count - 1;
-    int price = pricesSorted[idx];
+    var price = pricesSorted[idx];
 
     // 쿠폰을 사용했을 때의 결과
-    int yesCoupon = recursion(
+    var yesCoupon = recursion(
         pricesSorted,
-        remainBudget - (price / 2),
+        remainBudget - (int) (price / 2),
         remainCoupon - 1,
         idx + 1,
         count + 1);
@@ -143,14 +136,14 @@ class Solver25947Alt2 {
     if (yesCoupon < best)
       return best;
     // 쿠폰을 사용하지 않았을 때의 결과
-    int noCoupon = recursion(
+    var noCoupon = recursion(
         pricesSorted,
-        remainBudget - price,
+        remainBudget - (int) price,
         remainCoupon,
         idx + 1,
         count + 1);
 
-    int result = Math.max(noCoupon, yesCoupon);
+    long result = Math.max(noCoupon, yesCoupon);
     best = Math.max(best, result);
     return best;
   }
@@ -158,7 +151,7 @@ class Solver25947Alt2 {
 }
 
 class Solver25947Alt3 {
-  public static int solve(int[] prices, int budget, int coupon) {
+  public static int solve(long[] prices, int budget, int coupon) {
     Arrays.sort(prices);
     int max = 0;
     // 맨 뒤부터 할인을 때린다. 할인한 제품을 사든 말든 알바 아님
@@ -183,5 +176,75 @@ class Solver25947Alt3 {
     }
 
     return max;
+  }
+}
+
+class Solver25947Alt4 {
+  /**
+   * 파라메트릭 서치: n개의 선물을 구매할 수 있나요? => Y/N 결정문제로 바꿔서 업다운 게임을 할 수 있음.
+   * 1. 누적합을 만들면서 할인 없이 가장 많이 살 수 있는 max를 찾을 수 있다. 이때
+   * max...prices.length까지 업다운 게임을 실시
+   * 
+   * 2. 할인쿠폰을 사용하여 가장 많은 선물을 살 수 있는 경우를 리턴한다.
+   */
+  public static int solve(long[] prices, int budget, int coupon) {
+    Arrays.sort(prices);
+    // 1. 누적합
+    long[] acc = new long[prices.length];
+    int max = 0;
+    acc[0] = prices[0];
+    for (int i = 1; i < prices.length; ++i) {
+      acc[i] = acc[i - 1] + prices[i];
+    }
+    for (int i = 0; i < prices.length; ++i) {
+      if (budget < acc[i]) {
+        max = i - 1;
+        break;
+      }
+    }
+
+    // 2. up down game from range(max, prices.length)
+    int lo = max;
+    int hi = prices.length;
+    while (lo < hi) {
+      int mid = (lo + hi) / 2;
+      if (isPossible(acc, budget, coupon, mid)) {
+        // go right
+        lo = mid + 1;
+      } else {
+        // go left
+        hi = mid;
+      }
+    }
+    return hi - 1;
+  }
+
+  /**
+   * n개의 선물을 구매할 수 있나요?
+   * [n-coupon, n) 구간을 할인을 맥여보고 가능한지 판별한다.
+   * 1. Sum{i = 0}{n - coupon - 1}{prices[i]}
+   * = acc[n - coupon - 1]
+   * 는 원가로 구매한 선물들의 구간합
+   * 
+   * 2. Sum{i = n - coupon}{n - 1}{prices[i] / 2}
+   * = (acc[n - 1] - acc[n - coupon - 1]) / 2
+   * 는 할인가로 구매한 선물들의 구간합
+   */
+  public static boolean isPossible(long[] acc, int budget, int coupon, int n) {
+    long sum = 0;
+    if (n == 0) {
+      return true;
+    }
+    if (n <= coupon) {
+      // [0, n) / 2 모든 상품을 다 할인받을 수 있음.
+      sum += acc[n - 1] / 2;
+    } else {
+      // [0, n - coupon)
+      var unSaled = acc[n - coupon - 1];
+      // [n - coupon, n)
+      var saled = (acc[n - 1] - acc[n - coupon - 1]) / 2;
+      sum = unSaled + saled;
+    }
+    return sum > budget ? false : true;
   }
 }
--- acmicpc/Test25947.java
+++ acmicpc/Test25947.java
@@ -14,45 +14,57 @@ public class Test25947 {
   public void test1() {
     int b = 26;
     int a = 2;
-    int[] prices = new int[] { 4, 6, 2, 10, 8, 12 };
+    long[] prices = new long[] { 4, 6, 2, 10, 8, 12 };
     int answer = 5;
     assertEquals(answer, Solver25947.solve(prices, b, a));
     assertEquals(answer, Solver25947Alt1.solve(prices, b, a));
+    assertEquals(answer, Solver25947Alt2.solve(prices, b, a));
+    assertEquals(answer, Solver25947Alt3.solve(prices, b, a));
+    assertEquals(answer, Solver25947Alt4.solve(prices, b, a));
   }
 
   @Test
   public void test2() {
     int b = 23;
     int a = 1;
-    int[] prices = new int[] { 4, 6, 2, 12, 8, 14 };
+    long[] prices = new long[] { 4, 6, 2, 12, 8, 14 };
     int answer = 4;
     assertEquals(answer, Solver25947.solve(prices, b, a));
     assertEquals(answer, Solver25947Alt1.solve(prices, b, a));
+    assertEquals(answer, Solver25947Alt2.solve(prices, b, a));
+    assertEquals(answer, Solver25947Alt3.solve(prices, b, a));
+    assertEquals(answer, Solver25947Alt4.solve(prices, b, a));
   }
 
   @Test
-  @RepeatedTest(50)
+  @RepeatedTest(100)
   public void correctTest() {
-    int maxPrice = 500;
+    int maxPrice = 100;
     // int maxPrice = 100_000_001;
     Random r = new Random();
-    int n = 50;
+    int n = 70;
     // int n = 100000;
-    int b = r.nextInt(1, maxPrice + 1);
-    int a = r.nextInt(0, n + 1);
-    int[] prices = r.ints(2, maxPrice).filter(e -> e % 2 == 0).limit(n).toArray();
+    int b = r.nextInt(maxPrice + 1) + 1;
+    int a = r.nextInt(n + 1);
+    long[] prices = r.longs(2, maxPrice).filter(e -> e % 2 == 0).limit(n).toArray();
 
     int answer1 = Solver25947Alt1.solve(prices, b, a);
-    int answer2 = Solver25947Alt2.solve(prices, b, a);
-    int answer3 = Solver25947Alt3.solve(prices, b, a);
+    // int answer2 = Solver25947Alt2.solve(prices, b, a);
+    // int answer3 = Solver25947Alt3.solve(prices, b, a);
+    int answer4 = Solver25947Alt4.solve(prices, b, a);
 
     Arrays.sort(prices);
-    assertEquals(answer1, answer2, String.format("""
-        prices: %s
-        budget: %d
-        coupon: %d
-          """, Arrays.toString(prices), b, a));
-    assertEquals(answer1, answer3, String.format("""
+    // assertEquals(answer1, answer2, String.format("""
+    // prices: %s
+    // budget: %d
+    // coupon: %d
+    // """, Arrays.toString(prices), b, a));
+    // assertEquals(answer1, answer3, String.format("""
+    // prices: %s
+    // budget: %d
+    // coupon: %d
+    // """, Arrays.toString(prices), b, a));
+    assertEquals(answer1, answer4, String.format("""
         prices: %s
         budget: %d
         coupon: %d
@@ -61,22 +73,45 @@ public class Test25947 {
 
   @Test
   public void edgeTest() {
-    var prices = new int[] { 2, 16, 16, 22, 38, 46, 52, 60, 72, 80, 92 };
+    var prices = new long[] { 2, 16, 16, 22, 38, 46, 52, 60, 72, 80, 92 };
     var budget = 286;
     var coupon = 6;
 
-    int answer1 = Solver25947Alt1.solve(prices, budget, coupon);
-    int answer2 = Solver25947Alt2.solve(prices, budget, coupon);
-    int answer3 = Solver25947Alt3.solve(prices, budget, coupon);
+    // int answer1 = Solver25947Alt1.solve(prices, budget, coupon);
+    // int answer2 = Solver25947Alt2.solve(prices, budget, coupon);
+    // int answer3 = Solver25947Alt3.solve(prices, budget, coupon);
+    int answer4 = Solver25947Alt4.solve(prices, budget, coupon);
 
     Arrays.sort(prices);
-    assertEquals(answer1, answer2);
-    assertEquals(answer1, answer3);
+    // assertEquals(answer1, answer2);
+    // assertEquals(answer1, answer3);
+    // assertEquals(answer1, answer4);
+  }
+
+  @Test
+  public void edgeTest2() {
+    var prices = new long[] { 2, 2, 2, 2, 2, 1000000 };
+    var budget = 10;
+    var coupon = prices.length;
+    var answer = 5;
+
+    int submit = Solver25947Alt4.solve(prices, budget, coupon);
+    assertEquals(answer, submit);
+  }
+
+  @Test
+  public void edgeTest3() {
+    var prices = new long[] { 1_000_000_000, 1_000_000_000, 1_000_000_000, 1_000_000_000, };
+    var budget = 1_000_000_000;
+    var coupon = 2;
+    var answer = 2;
+    int submit = Solver25947Alt4.solve(prices, budget, coupon);
+    assertEquals(answer, submit);
   }
 
   @Test
   @Timeout(1)
-  @RepeatedTest(1)
+  @RepeatedTest(10)
   public void timeoutTest() {
     Random r = new Random();
 
@@ -84,13 +119,19 @@ public class Test25947 {
     // int maxPrice = 100001;
     int n = 100000;
     // int n = 7500;
-    int b = r.nextInt(1, maxPrice + 1);
-    int a = r.nextInt(0, n + 1);
+    int b = r.nextInt(maxPrice) + 1;
+    int a = r.nextInt(n + 1);
 
-    int[] prices = r.ints(2, maxPrice).filter(e -> e % 2 == 0).limit(n).toArray();
+    long[] prices = r.longs(2, maxPrice).filter(e -> e % 2 == 0).limit(n).toArray();
 
     // int answer1 = Solver25947Alt1.solve(prices, b, a);
     // int answer2 = Solver25947Alt2.solve(prices, b, a);
-    int answer3 = Solver25947Alt3.solve(prices, b, a);
+    // int answer3 = Solver25947Alt3.solve(prices, b, a);
+    int answer4 = Solver25947Alt4.solve(prices, b, a);
+  }
+
+  @Test
+  public void maxIntTest() {
+    System.out.println(Integer.MAX_VALUE);
   }
 }
