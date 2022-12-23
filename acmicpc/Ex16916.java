e027d46d2c12280c7d9ec651ad7537bb232659cf
ChoiWheatley <chltmdgus604@naver.com>
2022-12-24 00:06:42 +0900

    [16916] KMP를 활용한 부분문자열 [성공]

--- acmicpc/Ex16916.java
+++ acmicpc/Ex16916.java
@@ -13,39 +13,49 @@ public class Ex16916 {
       String a = br.readLine();
       String b = br.readLine();
 
-      boolean submit = Solver16916.solve(a, b);
-      bw.write(submit ? "1" : "0");
-      bw.write("\n");
-
+      boolean submit = Solution.solution(a, b);
+      bw.write((submit ? 1 : 0) + "\n");
     } catch (IOException e) {
       e.printStackTrace();
     }
   }
-}
 
-class Solver16916 {
-  public static boolean solve(String a, String b) {
-    char[] aArr = a.toCharArray();
-    char[] bArr = b.toCharArray();
-
-    return false;
-  }
-
-  /** 접두어, 접미어 일치여부 조사 */
-  public static int[] failFunction(String keyword) {
-    int[] ret = new int[keyword.length()];
-    for (int i = 0; i < keyword.length(); ++i) {
-      for (int len = 1; len <= i / 2; ++len) {
-        int rightStart = i - len;
-        int cnt = 0;
-        for (int j = 0; j < len; ++j) {
-          if (keyword.charAt(j) == keyword.charAt(rightStart + j))
-            cnt++;
+  public static class Solution {
+    /** b가 a의 부분문자열이면 true를 리턴한다. */
+    public static boolean solution(String a, String b) {
+      int[] fail = makeFail(b);
+      int j = 0;
+      for (int i = 0; i < a.length(); ++i) {
+        // 글자 불일치
+        while (j > 0 && a.charAt(i) != b.charAt(j)) {
+          j = fail[j - 1];
         }
-        if (cnt == len)
-          ret[i] = len;
+        // 글자 일치
+        if (a.charAt(i) == b.charAt(j)) {
+          if (j == b.length() - 1) {
+            return true;
+          } else {
+            j++;
+          }
+        }
       }
+      return false;
     }
-    return ret;
+
+    /** KMP 알고리즘을 활용하여 str 안에 접두어와 접미어가 중복되는 부분을 찾자. */
+    public static int[] makeFail(String str) {
+      int[] fail = new int[str.length()];
+      int j = 0;
+      for (int i = 1; i < str.length(); ++i) {
+        while (j > 0 && str.charAt(i) != str.charAt(j)) { // follow fail backward until j goes to 0 or some occurrences
+                                                          // is found
+          j = fail[j - 1];
+        }
+        if (str.charAt(i) == str.charAt(j)) { // occurrence was found
+          fail[i] = ++j;
+        }
+      }
+      return fail;
+    }
   }
 }
--- acmicpc/Test16916.java
+++ acmicpc/Test16916.java
@@ -5,7 +5,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.util.Arrays;
 
-import static acmicpc.Solver16916.*;
+import static acmicpc.Ex16916.Solution.*;
 
 import org.junit.Test;
 
@@ -15,7 +15,7 @@ public class Test16916 {
     String a = "baekjoon";
     String b = "aek";
     boolean answer = true;
-    boolean submit = Solver16916.solve(a, b);
+    boolean submit = solution(a, b);
     assertEquals(answer, submit);
   }
 
@@ -24,7 +24,7 @@ public class Test16916 {
     String a = "baekjoon";
     String b = "bak";
     boolean answer = false;
-    boolean submit = Solver16916.solve(a, b);
+    boolean submit = solution(a, b);
     assertEquals(answer, submit);
   }
 
@@ -33,15 +33,15 @@ public class Test16916 {
     String a = "baekjoon";
     String b = "joo";
     boolean answer = true;
-    boolean submit = Solver16916.solve(a, b);
+    boolean submit = solution(a, b);
     assertEquals(answer, submit);
   }
 
   @Test
   public void failureFunction1() {
     String keyword = "aaab";
-    int[] answer = { 0, 1, 1, 0 };
-    int[] submit = failFunction(keyword);
+    int[] answer = { 0, 1, 2, 0 };
+    int[] submit = makeFail(keyword);
     assertArrayEquals(answer, submit, String.format("""
         answer = %s
         submit = %s
