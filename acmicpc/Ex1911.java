4609da1f8717977e0c2b0419ceea2c45291d9000
ChoiWheatley <chltmdgus604@naver.com>
2023-01-10 16:47:08 +0900

    [1911] 구멍의 범위가 서로 겹치는 문제에 대해 고려자체를 안해 발생한 문제.

--- acmicpc/Ex1911.java
+++ acmicpc/Ex1911.java
@@ -15,27 +15,27 @@ public class Ex1911 {
         String[] line = reader.readLine().split(" ");
         int n = Integer.valueOf(line[0]);
         int l = Integer.valueOf(line[1]);
-        List<Hole> holes = new ArrayList<>(n);
+        List<Range> holes = new ArrayList<>(n);
         while (n-- > 0) {
             line = reader.readLine().split(" ");
             int start = Integer.valueOf(line[0]);
             int end = Integer.valueOf(line[1]);
-            holes.add(new Hole(start, end));
+            holes.add(new Range(start, end));
         }
 
-        int submit = Solution.solution(holes, l);
+        int submit = Solution2.solution(holes, l);
         System.out.println(submit);
 
         reader.close();
     }
 
     public static class Solution {
-        public static int solution(List<Hole> ls, int plankLen) {
+        public static int solution(List<Range> ls, int plankLen) {
             Collections.sort(ls);
             // 첫번째 구멍의 위치를 0번으로 전부 바꾼다.
             int offset = ls.get(0).start;
             ls = ls.stream().map((before) -> {
-                return new Hole(before.start - offset, before.end - offset);
+                return new Range(before.start - offset, before.end - offset);
             }).collect(Collectors.toList());
 
             int result = 0;
@@ -74,7 +74,7 @@ public class Ex1911 {
                 }
             }
             // Exceptional case: plankLen이 지나치게 커서 카운트를 세지 않고 넘어간 경우
-            Hole lastHole = ls.get(ls.size() - 1);
+            Range lastHole = ls.get(ls.size() - 1);
             if (start <= lastHole.start && lastHole.end <= end) {
                 result++;
             }
@@ -82,19 +82,109 @@ public class Ex1911 {
         }
     }
 
-    public static class Hole implements Comparable<Hole> {
+    public static class Solution2 {
+        /**
+         * 조건: 판자 pI..<pJ를 가지고 구멍 hI..<hJ를 막을 수 있나요?
+         * 증가하는 경우: 위의 조건이 TRUE가 될 때까지
+         */
+        public static int solution(List<Range> holes, int plankLen) {
+            Collections.sort(holes);
+            final int SIZE = holes.size();
+            final int BEGIN = holes.get(0).start;
+            final int LAST = holes.get(SIZE - 1).end;
+
+            int result = 0;
+
+            int idx = 0;
+            for (int pos = BEGIN; pos < LAST && idx < SIZE;) {
+                Range hole = holes.get(idx);
+                Range plank = Range.of(hole.start, hole.start);
+                do {
+                    if (plank.end < hole.end) {
+                        // plank를 더 붙여야 하는 경우
+                        int count = (int) Math.ceil((hole.end - plank.end) / (double) plankLen);
+                        int len = count * plankLen;
+                        result += count;
+                        plank = Range.of(plank.start, plank.end + len);
+                    }
+                    if (idx < SIZE - 1) {
+                        idx += 1;
+                        Range nextHole = holes.get(idx);
+                        if (nextHole.start <= plank.end) {
+                            // hole의 크기를 늘려야 하는 경우
+                            hole = Range.merge(hole, nextHole);
+                        }
+                    }
+                } while (!canCover(hole, plank));
+                // YES! plank can cover the hole
+                pos = plank.end;
+            }
+
+            return result;
+        }
+
+        /**
+         * 판자 pI..<pJ를 가지고 구멍 hI..<hJ를 막을 수 있나요?
+         * 
+         * @assertion subList는 정렬된 상태라 가정한다.
+         */
+        public static boolean canCover(Range hole, Range plank) {
+            return plank.start <= hole.start && hole.end <= plank.end;
+        }
+    }
+
+    public static class Solution3 {
+        public static int solution(List<Range> holes, int plankLen) {
+            Collections.sort(holes);
+
+            int result = 0;
+            int range = 0; // 판자의 범위
+            for (int idx = 0; idx < holes.size(); ++idx) {
+                Range hole = holes.get(idx);
+                if (hole.start > range) {
+                    range = hole.start;
+                }
+                if (hole.end >= range) {
+                    // 판자를 더 붙여야 한다.
+                    while (hole.end > range) {
+                        range += plankLen;
+                        result += 1;
+                    }
+                }
+            }
+            return result;
+        }
+    }
+
+    public static class Range implements Comparable<Range> {
 
         public final int start;
         public final int end;
 
-        public Hole(int startInclusive, int endExclusive) {
+        public Range(int startInclusive, int endExclusive) {
             this.start = startInclusive;
             this.end = endExclusive;
         }
 
+        public static Range of(int startInclusive, int endExclusive) {
+            return new Range(startInclusive, endExclusive);
+        }
+
         @Override
-        public int compareTo(Hole o) {
+        public int compareTo(Range o) {
+            if (this.start == o.start) {
+                return this.end - o.end;
+            }
             return this.start - o.start;
         }
+
+        public int len() {
+            return end - start;
+        }
+
+        public static Range merge(Range a, Range b) {
+            return Range.of(Integer.min(a.start, b.start), Integer.max(a.end, b.end));
+        }
     }
+
 }
--- acmicpc/Test1911.java
+++ acmicpc/Test1911.java
@@ -7,134 +7,268 @@ import java.util.List;
 
 import org.junit.Test;
 
-import acmicpc.Ex1911.Hole;
+import acmicpc.Ex1911.Range;
 import acmicpc.Ex1911.Solution;
+import acmicpc.Ex1911.Solution2;
+import acmicpc.Ex1911.Solution3;
 
 public class Test1911 {
     @Test
     public void sol1() {
-        /**
-         * 3 3
-         * 1 6
-         * 13 17
-         * 8 12
-         */
         int plankLen = 3;
-        List<Hole> holes = Arrays.asList(
-                new Hole(1, 6),
-                new Hole(13, 17),
-                new Hole(8, 12));
+        List<Range> holes = Arrays.asList(
+                new Range(1, 6),
+                new Range(13, 17),
+                new Range(8, 12));
         int answer = 5;
         int submit = Solution.solution(holes, plankLen);
+        int submit2 = Solution2.solution(holes, plankLen);
         assertEquals(answer, submit);
+        assertEquals(answer, submit2);
 
-        plankLen = 4;
-        answer = 4;
-        submit = Solution.solution(holes, plankLen);
-        assertEquals(answer, submit);
+        int submit3 = Solution3.solution(holes, plankLen);
+        assertEquals(answer, submit3);
+    }
 
-        plankLen = 5;
-        answer = 3;
-        submit = Solution.solution(holes, plankLen);
+    @Test
+    public void sol1$1() {
+        int plankLen = 4;
+        List<Range> holes = Arrays.asList(
+                new Range(1, 6),
+                new Range(13, 17),
+                new Range(8, 12));
+        int answer = 4;
+        int submit = Solution.solution(holes, plankLen);
+        int submit2 = Solution2.solution(holes, plankLen);
         assertEquals(answer, submit);
+        assertEquals(answer, submit2);
+        int submit3 = Solution3.solution(holes, plankLen);
+        assertEquals(answer, submit3);
+    }
+
+    @Test
+    public void sol1$2() {
+        List<Range> holes = Arrays.asList(
+                new Range(1, 6),
+                new Range(13, 17),
+                new Range(8, 12));
+        int plankLen = 5;
+        int answer = 3;
+        int submit = Solution.solution(holes, plankLen);
+        int submit2 = Solution2.solution(holes, plankLen);
+        assertEquals(answer, submit);
+        assertEquals(answer, submit2);
+        int submit3 = Solution3.solution(holes, plankLen);
+        assertEquals(answer, submit3);
+    }
+
+    @Test
+    public void sol1$3() {
+        List<Range> holes = Arrays.asList(
+                new Range(1, 6),
+                new Range(13, 17),
+                new Range(8, 12));
+        int plankLen = 6;
+        int answer = 3;
+        int submit = Solution.solution(holes, plankLen);
+        int submit2 = Solution2.solution(holes, plankLen);
+        // assertEquals(answer, submit);
+        assertEquals(answer, submit2);
+        int submit3 = Solution3.solution(holes, plankLen);
+        assertEquals(answer, submit3);
     }
 
     @Test
     public void sol2() {
-        int planklen = 1;
-        List<Hole> holes = Arrays.asList(
-                new Hole(0, 1),
-                new Hole(1_000_000_000 - 1, 1_000_000_000));
+        int plankLen = 1;
+        List<Range> holes = Arrays.asList(
+                new Range(0, 1),
+                new Range(1_000_000_000 - 1, 1_000_000_000));
         int answer = 2;
-        int submit = Solution.solution(holes, planklen);
+        int submit = Solution.solution(holes, plankLen);
+        int submit2 = Solution2.solution(holes, plankLen);
 
         assertEquals(answer, submit);
+        assertEquals(answer, submit2);
+        int submit3 = Solution3.solution(holes, plankLen);
+        assertEquals(answer, submit3);
     }
 
     @Test
     public void timeout1() {
-        int planklen = 1;
-        List<Hole> holes = Arrays.asList(
-                new Hole(0, 1_000_000_000));
+        int plankLen = 1;
+        List<Range> holes = Arrays.asList(
+                new Range(0, 1_000_000_000));
         int answer = 1_000_000_000;
-        int submit = Solution.solution(holes, planklen);
+        int submit = Solution.solution(holes, plankLen);
+        int submit2 = Solution2.solution(holes, plankLen);
 
         assertEquals(answer, submit);
+        assertEquals(answer, submit2);
+        int submit3 = Solution3.solution(holes, plankLen);
+        assertEquals(answer, submit3);
     }
 
     @Test
     public void sol3() {
-        int planklen = 10;
-        List<Hole> holes = Arrays.asList(
-                new Hole(0, 1),
-                new Hole(2, 3),
-                new Hole(4, 5),
-                new Hole(6, 7),
-                new Hole(8, 9));
+        List<Range> holes = Arrays.asList(
+                new Range(0, 1),
+                new Range(2, 3),
+                new Range(4, 5),
+                new Range(6, 7),
+                new Range(8, 9));
+        int plankLen = 10;
         int answer = 1;
-        int submit = Solution.solution(holes, planklen);
-        assertEquals(answer, submit);
+        int submit = Solution.solution(holes, plankLen);
+        int submit2 = Solution2.solution(holes, plankLen);
 
-        planklen = 2;
-        answer = 5;
-        submit = Solution.solution(holes, planklen);
         assertEquals(answer, submit);
+        assertEquals(answer, submit2);
+        int submit3 = Solution3.solution(holes, plankLen);
+        assertEquals(answer, submit3);
+    }
 
-        planklen = 1;
-        answer = 5;
-        submit = Solution.solution(holes, planklen);
-        assertEquals(answer, submit);
+    @Test
+    public void sol3$1() {
+        List<Range> holes = Arrays.asList(
+                new Range(0, 1),
+                new Range(2, 3),
+                new Range(4, 5),
+                new Range(6, 7),
+                new Range(8, 9));
+        int plankLen = 2;
+        int answer = 5;
+        int submit = Solution.solution(holes, plankLen);
+        int submit2 = Solution2.solution(holes, plankLen);
 
-        planklen = 100;
-        answer = 1;
-        submit = Solution.solution(holes, planklen);
         assertEquals(answer, submit);
+        assertEquals(answer, submit2);
+        int submit3 = Solution3.solution(holes, plankLen);
+        assertEquals(answer, submit3);
+    }
+
+    @Test
+    public void sol3$2() {
+        List<Range> holes = Arrays.asList(
+                new Range(0, 1),
+                new Range(2, 3),
+                new Range(4, 5),
+                new Range(6, 7),
+                new Range(8, 9));
+        int plankLen = 1;
+        int answer = 5;
+        int submit = Solution.solution(holes, plankLen);
+        int submit2 = Solution2.solution(holes, plankLen);
+
+        assertEquals(answer, submit);
+        assertEquals(answer, submit2);
+        int submit3 = Solution3.solution(holes, plankLen);
+        assertEquals(answer, submit3);
+    }
+
+    @Test
+    public void sol3$3() {
+        List<Range> holes = Arrays.asList(
+                new Range(0, 1),
+                new Range(2, 3),
+                new Range(4, 5),
+                new Range(6, 7),
+                new Range(8, 9));
+        int plankLen = 100;
+        int answer = 1;
+        int submit = Solution.solution(holes, plankLen);
+        int submit2 = Solution2.solution(holes, plankLen);
+
+        assertEquals(answer, submit);
+        assertEquals(answer, submit2);
+        int submit3 = Solution3.solution(holes, plankLen);
+        assertEquals(answer, submit3);
     }
 
     @Test
     public void sol4() {
-        int planklen = 10;
-        List<Hole> holes = Arrays.asList(
-                new Hole(1, 2),
-                new Hole(3, 4),
-                new Hole(5, 6),
-                new Hole(7, 8),
-                new Hole(9, 10));
+        int plankLen = 10;
+        List<Range> holes = Arrays.asList(
+                new Range(1, 2),
+                new Range(3, 4),
+                new Range(5, 6),
+                new Range(7, 8),
+                new Range(9, 10));
         int answer = 1;
-        int submit = Solution.solution(holes, planklen);
+        int submit = Solution.solution(holes, plankLen);
+        int submit2 = Solution2.solution(holes, plankLen);
+
         assertEquals(answer, submit);
+        assertEquals(answer, submit2);
+        int submit3 = Solution3.solution(holes, plankLen);
+        assertEquals(answer, submit3);
     }
 
     @Test
     public void sol5() {
-        int planklen = 1_000_000_000;
-        List<Hole> holes = Arrays.asList(
-                new Hole(0, 1));
+        int plankLen = 1_000_000_000;
+        List<Range> holes = Arrays.asList(
+                new Range(0, 1));
         int answer = 1;
-        int submit = Solution.solution(holes, planklen);
+        int submit = Solution.solution(holes, plankLen);
+        int submit2 = Solution2.solution(holes, plankLen);
+
         assertEquals(answer, submit);
+        assertEquals(answer, submit2);
+        int submit3 = Solution3.solution(holes, plankLen);
+        assertEquals(answer, submit3);
     }
 
     @Test
     public void sol6() {
-        int planklen = 10;
-        List<Hole> holes = Arrays.asList(
-                new Hole(0, 1),
-                new Hole(10, 11));
+        int plankLen = 10;
+        List<Range> holes = Arrays.asList(
+                new Range(0, 1),
+                new Range(10, 11));
         int answer = 2;
-        int submit = Solution.solution(holes, planklen);
+        int submit = Solution.solution(holes, plankLen);
+        int submit2 = Solution2.solution(holes, plankLen);
+
         assertEquals(answer, submit);
+        assertEquals(answer, submit2);
+        int submit3 = Solution3.solution(holes, plankLen);
+        assertEquals(answer, submit3);
     }
 
     @Test
     public void sol7() {
-        int planklen = 10;
-        List<Hole> holes = Arrays.asList(
-                new Hole(2, 7),
-                new Hole(9, 10),
-                new Hole(11, 12));
+        int plankLen = 10;
+        List<Range> holes = Arrays.asList(
+                new Range(2, 7),
+                new Range(9, 10),
+                new Range(11, 12));
         int answer = 1;
-        int submit = Solution.solution(holes, planklen);
+        int submit = Solution.solution(holes, plankLen);
+        int submit2 = Solution2.solution(holes, plankLen);
+
         assertEquals(answer, submit);
+        assertEquals(answer, submit2);
+        int submit3 = Solution3.solution(holes, plankLen);
+        assertEquals(answer, submit3);
+    }
+
+    @Test
+    public void sol8() {
+        int plankLen = 1;
+        List<Range> holes = Arrays.asList(
+                Range.of(0, 2),
+                Range.of(1, 3),
+                Range.of(2, 4),
+                Range.of(3, 5),
+                Range.of(4, 6),
+                Range.of(5, 7));
+        int answer = 6;
+        int submit = Solution.solution(holes, plankLen);
+        int submit2 = Solution2.solution(holes, plankLen);
+
+        assertEquals(answer, submit);
+        assertEquals(answer, submit2);
+        int submit3 = Solution3.solution(holes, plankLen);
+        assertEquals(answer, submit3);
     }
 }
