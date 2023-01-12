d1a73b3c448158098461c0ed64abe9a1fef38f57
ChoiWheatley <chltmdgus604@naver.com>
2023-01-12 18:14:24 +0900

    [2170] Sweeping 알고리즘 다른 블로그 글 참조함.

--- acmicpc/Ex2170.java
+++ acmicpc/Ex2170.java
@@ -5,7 +5,10 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
 import java.util.Set;
 import java.util.TreeMap;
 import java.util.TreeSet;
@@ -15,25 +18,39 @@ import useful.Pair;
 public class Ex2170 {
     public static void main(String[] args) {
 
-        Solution solution = new Solution();
-
         try (var br = new BufferedReader(new InputStreamReader(System.in));
                 var bw = new BufferedWriter(new OutputStreamWriter(System.out))) {
             String line = br.readLine();
             int t = Integer.parseInt(line);
 
+            /** Solution1 방식 */
+            // Solution solution = new Solution();
+            // while (t-- > 0) {
+            // line = br.readLine();
+            // String[] splitted = line.split(" ");
+            // int start = Integer.parseInt(splitted[0]);
+            // int end = Integer.parseInt(splitted[1]);
+
+            // solution.add(Line.of(start, end));
+            // }
+
+            // Collection<Line> lines = solution.getLines();
+            // int sum = lines.stream().mapToInt(Line::length).sum();
+            // bw.write(sum + "\n");
+
+            /** Solution2 방식 */
+            List<Line> lines = new ArrayList<>(t);
             while (t-- > 0) {
+
                 line = br.readLine();
                 String[] splitted = line.split(" ");
                 int start = Integer.parseInt(splitted[0]);
                 int end = Integer.parseInt(splitted[1]);
 
-                solution.add(Line.of(start, end));
+                lines.add(Line.of(start, end));
             }
-
-            Collection<Line> lines = solution.getLines();
-            int sum = lines.stream().mapToInt(Line::length).sum();
-            bw.write(sum + "\n");
+            int submit = Solution2.solution(lines);
+            bw.write(submit + "\n");
 
         } catch (IOException e) {
             e.printStackTrace();
@@ -111,7 +128,35 @@ public class Ex2170 {
             result.retainAll(e);
             return result;
         }
+    }
 
+    /**
+     * <h1>sweeping algorithm 사용한 풀이법</h1>
+     * 
+     * https://coder-in-war.tistory.com/entry/%EA%B0%9C%EB%85%90-47-%EC%8A%A4%EC%9C%84%ED%95%91-%EC%95%8C%EA%B3%A0%EB%A6%AC%EC%A6%98Sweeping-Algorithm
+     */
+    public static class Solution2 {
+        public static int solution(List<Line> lines) {
+
+            Collections.sort(lines); // 수평선을 한 방향으로만 훑을 수 있게
+            int result = 0;
+            int l = Integer.MIN_VALUE; // l, r : range of interest
+            int r = Integer.MIN_VALUE;
+            for (Line e : lines) {
+
+                if (r < e.start()) { // 병합 불가 조건
+
+                    result += r - l; // 우리는 길이만을 원한다.
+                    l = e.start(); // 현재 구간을 초기화, 오로지 우리는 오른쪽으로만 간다.
+                    r = e.end();
+                } else { // 병합 가능 조건
+                    r = Integer.max(r, e.end());
+                }
+            }
+            result += r - l; // last case
+
+            return result;
+        }
     }
 
     /** 값 객체 */
